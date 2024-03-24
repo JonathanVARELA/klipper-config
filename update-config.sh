597b8f3d8e838a6aa628d20dddcae550d9ba2207
Jonathan VARELA <jonathan.varela29@gmail.com>
2024-03-24 14:09:37 +0100

    fix: update-config script

--- update-config.sh
+++ update-config.sh
@@ -2,7 +2,6 @@
 git pull
 
 # this script is located at the root of the project
-# do not use zenity, use native bash dialog boxes
 # each folder at the root of the project represent a config file for a given printer (ex: a10m, ender3, etc...)
 # first of all, open a dialog box to select the printer config file to update
 # then, check if the folder with the same name exists in the home directory ex: ~/a10m
@@ -17,23 +16,31 @@ git pull
 # Code :
 
 # Select a printer config file
-printer_cfg=$(dialog --stdout --title "Select a printer config file" --fselect /home/pi/ 14 48)
-# Check if the folder with the same name exists in the home directory
-printer_folder=$(basename $printer_cfg)
-if [ ! -d ~/printer_folder ]; then
-    mkdir ~/printer_folder
-fi
-# Check if the file printer.cfg exists in the folder ~/
-if [ ! -f ~/printer.cfg ]; then
-    cp $printer_folder/printer.cfg ~/printer.cfg
-fi
-# Check if the folder config exists in the folder ~/{printer_folder}
-if [ ! -d ~/printer_folder/config ]; then
-    mkdir ~/printer_folder/config
-fi
-# Clear the contents of the folder ~/{printer_folder}/config
-rm -rf ~/printer_folder/config/*
-# Copy the contents of the project config folder to the newly created config folder
-cp -r $printer_folder/config/* ~/printer_folder/config/
-# Send a message to the user that the update is complete
-dialog --title "Update Complete" --msgbox "The update is complete" 10 40
+items=$(ls -d */ | sed 's/\///g')
+# print items
+
+while [ choice=$(dialog --title "Select your printer" \
+  --menu "Choose one of the following printer:" 10 40 3 \
+  "${items[@]}" \
+  2>&1 >/dev/tty
+) ]; do
+  # echo $choice
+  # Check if the folder with the same name exists in the home directory
+  if [ ! -d ~/$choice ]; then
+    mkdir ~/$choice
+  fi
+  # Check if the file printer.cfg exists in the folder ~/
+  if [ ! -f ~/$choice/printer.cfg ]; then
+    cp $choice/printer.cfg ~/$choice/printer.cfg
+  fi
+  # Check if the folder config exists in the folder ~/a10m
+  if [ ! -d ~/$choice/config ]; then
+    mkdir ~/$choice/config
+  fi
+  # Clear the contents of the folder ~/a10m/config
+  rm -rf ~/$choice/config/*
+  # Copy the contents of the project config folder to the newly created config folder
+  cp -r $choice/config/* ~/$choice/config/
+  # Send a message to the user that the update is complete
+  dialog --title "Update complete" --msgbox "The update is complete" 10 40
+  break
